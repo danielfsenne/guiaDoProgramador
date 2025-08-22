4eb36c33c786a2abef7797a82466e66a06b4cc24
danielSenne <danielflausinosenne123@gmail.com>
2025-08-22 12:59:47 -0300

    knex, SQL avançado

---
+++ banco/index.js
@@ -0,0 +1,47 @@
+var database = require('./database')
+
+var dados = [
+  { name: "Sea of Thieves", price: 50.67 },
+  { name: "Silent Hill 2", price: 60 }
+]
+
+database.insert(dados).into("games")
+  .then(() => console.log("Games inseridos com sucesso!"))
+  .catch(err => console.log(err))
+
+database.select(["idgames", "price"]).table("games")
+  .then(data => console.log("Lista de games:", data))
+  .catch(err => console.log(err))
+
+database.insert({ name: "Mists of noyah", price: 26 }).into("games")
+  .then(() => {
+    return database.select(["idgames", "price"]).table("games")
+  })
+  .then(data => console.log("Lista atualizada:", data))
+  .catch(err => console.log(err))
+
+ database.select()
+    .whereRaw("name = 'Mists of Noyah'OR price > 50")
+    .table("games").then(data => {
+        console.log(data)
+    }).catch(err => {
+        console.log(err)
+    })
+
+database.where({idgames: 3}).delete().table("games").then(data => {
+    console.log(data)
+}).catch(err => {
+    console.log(err)
+})
+
+database.where({idgames: 5}).update({price: 20}).table("games").then(data => {
+    console.log(data)
+}).catch(err => {
+    console.log(err)
+})
+
+database.select().table("games").orderBy("price", "desc").then( data => {
+    console.log(data)
+}).catch(err => {
+    console.log(err)
+})
