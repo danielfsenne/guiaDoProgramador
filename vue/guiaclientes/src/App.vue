8a189d484c7d75b6d94b13f0fab39b03730a7b2e
danielSenne <danielflausinosenne123@gmail.com>
2025-08-25 12:13:12 -0300

    Introdução ao framework

---
+++ vue/guiaclientes/src/App.vue
@@ -0,0 +1,25 @@
+
+<template>
+  <div id="app">
+    <h1>Guia do programador</h1>
+    <ClientePage/>
+    <h4>Lista de Produtos</h4>
+    <ProdutoPage/>
+  </div>
+</template>
+
+<script>
+import ClientePage from './components/ClientePage.vue';
+import ProdutoPage from './components/ProdutoPage.vue';
+export default {
+  name: 'App',
+  components:{
+    ClientePage,
+    ProdutoPage
+  }
+}
+</script>
+  
+<style>
+
+</style>
---
+++ vue/guiaclientes/src/components/ProdutoPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Produto</h1>
+        <p>bla bla bla</p>
+        <input type="text" placeholder="Preço">
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+
+</style>
