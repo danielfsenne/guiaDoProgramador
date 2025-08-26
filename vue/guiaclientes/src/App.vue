0effc93fe119358231ee75f37080f8f52c896bd1
danielSenne <danielflausinosenne123@gmail.com>
2025-08-26 11:19:56 -0300

    diretivas condicionais

--- vue/guiaclientes/src/App.vue
+++ vue/guiaclientes/src/App.vue
@@ -2,20 +2,29 @@
 <template>
   <div id="app">
     <h1>Guia do programador</h1>
-    <ClientePage/>
-    <h4>Lista de Produtos</h4>
-    <ProdutoPage/>
+    <input type="text" v-model="nomeDoDaniel">
+    <ClientePage :nome="nomeDoDaniel" email="tuntuntun@gmail" idade="35" :showIdade="true"/>
+    <ClientePage nome="James da Salada de Fruta"  email="tuntuntun1@gmail" idade="30" showIdade="false"/>
+    <ClientePage nome="Tralalelo Tralala"  email="tuntuntun2@gmail" idade="67" :showIdade="false"/>
+    <ClientePage nome="Tuntuntun Sauhr"  email="tuntuntun3@gmail" idade="23" :showIdade="true"/>
+    <ClientePage nome="Bombardile Crocodile"  email="tuntuntun5@gmail" idade="344" :showIdade="true"/>
+    
   </div>
 </template>
 
 <script>
 import ClientePage from './components/ClientePage.vue';
-import ProdutoPage from './components/ProdutoPage.vue';
+//import ProdutoPage from './components/ProdutoPage.vue';
 export default {
   name: 'App',
+  data() {
+    return {
+      nomeDoDaniel: "D.Flausino Senne"
+    }
+  },
   components:{
     ClientePage,
-    ProdutoPage
+    // ProdutoPage
   }
 }
 </script>
--- vue/guiaclientes/src/components/ClientePage.vue
+++ vue/guiaclientes/src/components/ClientePage.vue
@@ -1,13 +1,44 @@
 <template>
-    <div>
-        <h2>Cliente Aqui</h2>
+    <div :class="{'cliente' : !isPremium, 'cliente-premium': isPremium}">
+        <h4>Nome: {{ nome }}</h4>
+        <hr>
+        <hr>
+        <p>Email: {{ email }}</p>
+        <p v-if="showIdade == true">Idade: {{ idade }}</p>
+        <p v-else>O usuário escondeu a idade</p>
     </div>
 </template>
 <script>
-    export default {
-
+    export default {   
+        data(){
+            return{
+                isPremium: false
+            }
+        },
+        props: {
+            nome: String,
+            email: String,
+            idade: Number,
+            showIdade: Boolean
+        }
     }
 </script>
-<style>
-
+<style scoped>
+    .cliente{
+        background-color: #ECE5E3;
+        max-height: 600px;
+        height: 180px;
+        padding: 1%;
+        margin-top: 2%;
+    }
+    .cliente-premium{
+        background-color: black;
+        color: yellow;
+        max-width: 600px;
+        height: 180px;
+        padding: 1%;
+        margin-top: 2%;
+    }
+    
 </style>
+
