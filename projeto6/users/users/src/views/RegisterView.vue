97d2e92e6daa24d081075fc17cb70a523a16d579
danielSenne <danielflausinosenne123@gmail.com>
2025-09-07 15:59:48 -0300

    Projeto integrando Vue.js e Node

---
+++ projeto6/users/users/src/views/RegisterView.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <h2>Registro de Usuário</h2>
+        <hr>
+        <div class="columns is-centered">
+            <div class="column is-half">
+                <p>Nome</p>
+                <input type="text" placeholder="Nome do Usuário" class="input" v-model="name">
+                <p>E-mail</p>
+                <input type="email" placeholder="email@emmail.com" class="input" v-model="email">
+                <p>Senha</p>
+                <input type="password" placeholder="******" class="input" v-model="password">
+                <hr>
+                <button class="button is-success " @click="register">Cadastrar</button>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default{
+    name: 'RegisterView',
+    data(){
+        return {
+            name: '',
+            password: '',
+            email: ''
+        }
+    },
+    methods:{
+        register(){
+         console.log(this.name)
+         console.log(this.email)
+         console.log(this.password)
+        }
+    },
+    
+}
+</script>
+<style scoped>
+
+</style>
